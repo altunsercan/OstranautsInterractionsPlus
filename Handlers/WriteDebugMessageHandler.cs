68e0d561c2d5b5227997201a07eeab5e577d1042
Sercan Altun <altun.sercan@gmail.com>
2020-12-22 20:28:28 -0500

    Add debug action for interactions

---
+++ Handlers/WriteDebugMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using InteractionsPlus.UI.IMGUI;
+
+namespace InteractionsPlus.Handlers
+{
+    public class WriteDebugMessageHandler
+    {
+        [InteractionHandler]
+        public static void WriteDebug(string message)
+        {
+            if (!InteractionsPlusMod.Services.TryResolve(out IMGUIExecutor executor) || executor == null)
+            {
+                return;
+            }
+            executor.AddHandler(new DebugMessageIMGUI(executor, message));
+        }
+    }
+}
