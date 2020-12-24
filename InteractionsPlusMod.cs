054b58c7af97517acbb59601cf23f8eecab6969a
Sercan Altun <altun.sercan@gmail.com>
2020-12-24 00:08:15 -0500

    Add dependency manager to access other mods

--- InteractionsPlusMod.cs
+++ InteractionsPlusMod.cs
@@ -1,6 +1,8 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using HarmonyLib;
 using InteractionsPlus.Handlers;
+using InteractionsPlus.ModDependency;
 using InteractionsPlus.UI.IMGUI;
 using JetBrains.Annotations;
 
@@ -8,6 +10,8 @@ namespace InteractionsPlus
 {
     public class InteractionsPlusMod
     {
+        public const string ModId = "InteractionsPlus";
+        
         [NotNull] public static readonly ServiceLocator Services = new ServiceLocator(); 
         
         [NotNull] private readonly ILogger logger;
@@ -17,12 +21,14 @@ namespace InteractionsPlus
         [NotNull] private readonly HandlerManager handlerManager;
         [NotNull] private readonly InteractionAdditionalActionsManager additionalDataManager;
         
+        [NotNull] private readonly UMMDependencyManager dependencyManager;
         internal InteractionsPlusMod([NotNull]ILogger logger)
         {
             this.logger = logger;
             handlerManager = new HandlerManager(logger);
             additionalDataManager = new InteractionAdditionalActionsManager();
             immediateGUIExecutor = new IMGUIExecutor();
+            dependencyManager = new UMMDependencyManager(logger);
             harmony = new Harmony("com.ostranauts.marchingninja.interactionsplus");
 
             Services.SetLogger(logger);
@@ -30,6 +36,7 @@ namespace InteractionsPlus
             Services.Bind<HandlerManager>(handlerManager);
             Services.Bind<InteractionAdditionalActionsManager>(additionalDataManager);
             Services.Bind<IMGUIExecutor>(immediateGUIExecutor);
+            Services.Bind<UMMDependencyManager>(dependencyManager);
         }
         
         public bool OnToggle(bool toggle)
